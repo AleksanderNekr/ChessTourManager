0c3494d473f11afeb32325ad4c07dd3c2242322a
aleks <aleksandr_nekrasov@outlook.com>
2023-02-22 10:30:41 +0500

    Added selected tournament tab.

--- ChessTourManagerWpf/ViewModels/TournamentsListViewModel.cs
+++ ChessTourManagerWpf/ViewModels/TournamentsListViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using ChessTourManagerWpf.Models;
 using ChessTourManagerWpf.Models.Entities;
 using CommunityToolkit.Mvvm.ComponentModel;
@@ -12,6 +13,46 @@ public partial class TournamentsListViewModel : ObservableObject
 {
     private static readonly ChessTourContext ChessTourContext = new();
 
-    [ObservableProperty] private ObservableCollection<Tournament> _tournamentsCollection =
+    [ObservableProperty]
+    private ObservableCollection<Tournament> _tournamentsCollection =
         new(ChessTourContext.Tournaments.Select(tournament => tournament));
+
+
+    [ObservableProperty]
+    private Tournament? _selectedTournament;
+
+    private string? _selectedTournamentName;
+
+    public string SelectedTournamentName
+    {
+        get
+        {
+            if (_selectedTournamentName == null)
+            {
+                _selectedTournamentName = "";
+                OnPropertyChanged();
+            }
+
+            return _selectedTournamentName;
+        }
+        private set
+        {
+            _selectedTournamentName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    [ObservableProperty]
+    private bool _isOpened;
+
+    [RelayCommand]
+    private void OpenTournament(object obj)
+    {
+        if (obj is Tournament tournament)
+        {
+            SelectedTournament     = tournament;
+            SelectedTournamentName = tournament.TournamentName;
+            IsOpened               = true;
+        }
+    }
 }
