ef3af8d6ce1bfdf719e3766654f53c115068957c
aleks <aleksandr_nekrasov@outlook.com>
2023-02-28 23:27:19 +0500

    Added commit changes of the players grid on focus change.

--- ChessTourManager.WPF/Views/Controls/PlayersGridControl.xaml.cs
+++ ChessTourManager.WPF/Views/Controls/PlayersGridControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using ChessTourManager.WPF.ViewModels;
 
@@ -12,4 +13,9 @@ public partial class PlayersGridControl
         PlayersViewModel.PlayersContext.SaveChanges();
 
     private void DataGrid_LoadingRow(object? sender, DataGridRowEventArgs e) => e.Row.Header = e.Row.GetIndex() + 1;
+
+    private void DataGrid_LostFocus(object sender, RoutedEventArgs e)
+    {
+        PlayersViewModel.PlayersContext.SaveChanges();
+    }
 }
