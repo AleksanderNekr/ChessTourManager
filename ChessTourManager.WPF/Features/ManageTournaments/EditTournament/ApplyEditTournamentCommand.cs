2b842e819283121992a517d85318d017c1238735
aleks <aleksandr_nekrasov@outlook.com>
2023-03-03 21:38:48 +0500

    Added confirmation of an edit.

--- ChessTourManager.WPF/Features/ManageTournaments/EditTournament/ApplyEditTournamentCommand.cs
+++ ChessTourManager.WPF/Features/ManageTournaments/EditTournament/ApplyEditTournamentCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using ChessTourManager.WPF.Helpers;
 
 namespace ChessTourManager.WPF.Features.ManageTournaments.EditTournament;
@@ -7,13 +8,23 @@ public class ApplyEditTournamentCommand : CommandBase
 {
     public override void Execute(object? parameter)
     {
-        TournamentsListViewModel.TournamentsListContext.SaveChanges();
-        if (EditTournamentViewModel.EditingTournament != null)
+        MessageBoxResult result = MessageBox.Show("Вы уверены, что хотите подтвердить редактирование турнира?",
+                                                  "Редактирование турнира",
+                                                  MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+        if (EditTournamentViewModel.EditingTournament == null || result != MessageBoxResult.Yes)
         {
-            MessageBox.Show("Турнир успешно отредактирован!", "Редактирвонаие турнира",
-                            MessageBoxButton.OK, MessageBoxImage.Information);
-            TournamentEditedEvent.OnTournamentEdited(new TournamentEditedEventArgs(EditTournamentViewModel
-                                                        .EditingTournament));
+            return;
         }
+
+        EditTournamentViewModel.EditingTournament.DateLastChange = DateOnly.FromDateTime(DateTime.UtcNow);
+        EditTournamentViewModel.EditingTournament.TimeLastChange = TimeOnly.FromDateTime(DateTime.UtcNow);
+        TournamentsListViewModel.TournamentsListContext.SaveChanges();
+
+        MessageBox.Show("Турнир успешно отредактирован!", "Редактирвонаие турнира",
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+
+        TournamentEditedEvent.OnTournamentEdited(new TournamentEditedEventArgs(EditTournamentViewModel
+                                                    .EditingTournament));
     }
 }
