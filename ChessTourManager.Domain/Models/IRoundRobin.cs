0e9ed8c98ebb5f0ee2a56507fb7222adbd42aa8f
aleks <aleksandr_nekrasov@outlook.com>
2023-03-10 19:57:27 +0500

    Added pseudo round-robin algorithm.

---
+++ ChessTourManager.Domain/Models/IRoundRobin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ChessTourManager.DataAccess;
+using ChessTourManager.DataAccess.Entities;
+
+namespace ChessTourManager.Domain.Models;
+
+public interface IRoundRobin
+{
+    public static IRoundRobin Initialize(ChessTourContext context, Tournament tournament) =>
+        new RoundRobin(context, tournament);
+
+    public IEnumerable<(int, int)> StartNewTour(int currentTour);
+
+    public HashSet<(int, int)> GamesHistory { get; }
+
+    public int NewTourNumber { get; }
+}
---
+++ ChessTourManager.Domain/Models/RoundRobin.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using ChessTourManager.DataAccess;
+using ChessTourManager.DataAccess.Entities;
+using ChessTourManager.DataAccess.Queries.Get;
+
+namespace ChessTourManager.Domain.Models;
+
+public class RoundRobin : IRoundRobin
+{
+    public RoundRobin(ChessTourContext context, Tournament tournament)
+    {
+        IGetQueries.CreateInstance(context)
+                   .TryGetGames(tournament.OrganizerId, tournament.TournamentId, out IEnumerable<Game>? games);
+        if (games is not null)
+        {
+            GamesHistory = games.Select(g => (g.WhiteId, g.BlackId)).ToHashSet();
+        }
+
+        IGetQueries.CreateInstance(context)
+                   .TryGetPlayers(tournament.OrganizerId, tournament.TournamentId,
+                                  out IEnumerable<Player>? players);
+
+        PlayersIds = players?.Select(p => p.PlayerId).ToList() ?? new List<int>();
+
+        ConfigureTours();
+    }
+
+    private void ConfigureTours()
+    {
+        for (var tourNumber = 1; tourNumber <= PlayersIds.Count / 2; tourNumber++)
+        {
+            _pairsForTour.TryAdd(tourNumber, ConfigurePairs());
+
+            // Rotate players, so that the first player is always the same
+            PlayersIds.Add(PlayersIds[0]);
+            PlayersIds.RemoveAt(0);
+        }
+    }
+
+    private Dictionary<int, HashSet<(int, int)>> _pairsForTour = new();
+
+    private HashSet<(int, int)> ConfigurePairs()
+    {
+        PlayersIds.Sort();
+
+        IEnumerable<int> whiteIds = PlayersIds.Take(PlayersIds.Count / 2);
+        IEnumerable<int> blackIds = PlayersIds.Skip(PlayersIds.Count / 2).Reverse();
+
+        return new HashSet<(int, int)>(whiteIds.Zip(blackIds, (w, b) => (w, b)));
+    }
+
+    private List<int> PlayersIds { get; }
+
+    public IEnumerable<(int, int)> StartNewTour(int currentTour)
+    {
+        NewTourNumber = currentTour + 1;
+        return _pairsForTour[currentTour + 1].Where(p => !GamesHistory.Contains(p));
+    }
+
+    public HashSet<(int, int)> GamesHistory { get; }
+
+    public int NewTourNumber { get; set; }
+}
