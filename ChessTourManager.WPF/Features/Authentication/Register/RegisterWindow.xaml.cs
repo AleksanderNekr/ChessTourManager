f1b6f4fe205842dba60be6382f760af435577dd8
aleks <aleksandr_nekrasov@outlook.com>
2023-03-19 19:24:28 +0500

    Feature: added registration.

--- ChessTourManager.WPF/Features/Authentication/Register/RegisterWindow.xaml.cs
+++ ChessTourManager.WPF/Features/Authentication/Register/RegisterWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Windows;
+using ChessTourManager.WPF.Features.Authentication.Login;
 using ChessTourManager.WPF.Features.ManageTournaments;
 
 namespace ChessTourManager.WPF.Features.Authentication.Register;
@@ -11,11 +13,23 @@ public partial class RegisterWindow : Window
     public RegisterWindow()
     {
         InitializeComponent();
+        SuccessRegisterEvent.UserSuccessRegister += RegisterViewModel_UserSuccessRegister;
     }
 
-    private void CompleteRegisterButton_Click(object sender, RoutedEventArgs e)
+    private void RegisterViewModel_UserSuccessRegister(SuccessRegisterEventArgs e)
     {
-        new MainWindow().Show();
+        new AuthWindow().Show();
         Close();
+    }
+
+    private void ReturnToAuthWindow_Click(object sender, RoutedEventArgs e)
+    {
+        new AuthWindow().Show();
+        Close();
+    }
+
+    private void RegisterWindow_Closing(object? sender, CancelEventArgs e)
+    {
+        SuccessRegisterEvent.UserSuccessRegister -= RegisterViewModel_UserSuccessRegister;
     }
 }
---
+++ ChessTourManager.WPF/Features/Authentication/Register/SuccessRegisterEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ChessTourManager.DataAccess.Entities;
+
+namespace ChessTourManager.WPF.Features.Authentication.Register;
+
+public static class SuccessRegisterEvent
+{
+    public delegate void UserSuccessRegisterHandler(SuccessRegisterEventArgs e);
+
+    public static event UserSuccessRegisterHandler? UserSuccessRegister;
+
+    internal static void OnUserSuccessRegister(SuccessRegisterEventArgs e)
+    {
+        UserSuccessRegister?.Invoke(e);
+    }
+}
+
+public class SuccessRegisterEventArgs : EventArgs
+{
+    public DateTimeOffset RegisterDateTimeUtc;
+    public User?          User;
+
+    public SuccessRegisterEventArgs(User? user, DateTimeOffset registerDateTimeUtc)
+    {
+        User                = user;
+        RegisterDateTimeUtc = registerDateTimeUtc;
+    }
+}
