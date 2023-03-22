4db36b2c8f40d556eca326d42a634c649ac59491
aleks <aleksandr_nekrasov@outlook.com>
2023-03-22 12:20:50 +0500

    Added integration tests for drawing system.

---
+++ ChessTourManager.IntegrationTests/FullDrawingTest.cs
@@ -0,0 +1,93 @@
+using ChessTourManager.DataAccess;
+using ChessTourManager.DataAccess.Entities;
+using ChessTourManager.DataAccess.Queries.Get;
+using ChessTourManager.DataAccess.Queries.Insert;
+using ChessTourManager.Domain.Algorithms;
+using ChessTourManager.WPF.Features.Authentication.Login;
+using ChessTourManager.WPF.Features.ManageTournaments;
+using ChessTourManager.WPF.Features.ManageTournaments.ManageGames;
+using ChessTourManager.WPF.Features.ManageTournaments.ManagePlayers;
+using ChessTourManager.WPF.Features.ManageTournaments.ManagePlayers.AddPlayer;
+using ChessTourManager.WPF.Features.ManageTournaments.OpenTournament;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace ChessTourManager.IntegrationTests;
+
+public class Tests
+{
+    private int              _orgId   = 2;
+    private int              _tourId  = 66;
+    private ChessTourContext _context = new();
+
+    [Test]
+    public void Test1()
+    {
+        // Get tournament.
+        IGetQueries.CreateInstance(_context)
+                   .TryGetTournaments(_orgId, out IEnumerable<Tournament>? tournaments);
+        Tournament tournament = tournaments?.Single(t => t.TournamentId == _tourId)
+                             ?? throw new InvalidOperationException();
+
+        // Create mock view model.
+        var viewModel = new PairsGridViewModel();
+
+        // Create mock TournamentsList view model.
+        var tournamentsListViewModel = new TournamentsListViewModel();
+
+        // Invoke open tournament command.
+        var openTournamentCommand = new OpenTournamentCommand(tournamentsListViewModel);
+        openTournamentCommand.Execute(tournament);
+
+        int oldTourNumber = viewModel.CurrentTour;
+
+        // Execute start tour command.
+        viewModel.StartNewTour.Execute(null);
+
+        // Check result.
+        Assert.AreEqual(oldTourNumber + 1, viewModel.CurrentTour);
+    }
+
+    [Test]
+    public void Test2()
+    {
+        // Get tournament.
+        IGetQueries.CreateInstance(_context)
+                   .TryGetTournaments(_orgId, out IEnumerable<Tournament>? tournaments);
+        Tournament tournament = tournaments?.Single(t => t.TournamentId == _tourId)
+                             ?? throw new InvalidOperationException();
+
+        // Create mock login view model.
+        var loginViewModel = new LoginViewModel
+                             {
+                                 Login    = tournament.Organizer.Email,
+                                 Password = "123qwe"
+                             };
+        var loginCommand = new LoginCommand(loginViewModel);
+
+        // Create mock pairs view model.
+        var viewModel = new PairsGridViewModel();
+        // Create mock TournamentsList view model.
+        var tournamentsListViewModel = new TournamentsListViewModel();
+
+        // Invoke login command.
+        loginCommand.Execute(null);
+
+        // Invoke open tournament command.
+        var openTournamentCommand = new OpenTournamentCommand(tournamentsListViewModel);
+        openTournamentCommand.Execute(tournament);
+
+
+        // Add new player.
+        IInsertQueries.CreateInstance(_context)
+                      .TryAddPlayer(out Player? _,               _tourId, _orgId,
+                                    "Test" + DateTime.Now.Ticks, "Player" + DateTime.Now.Ticks);
+
+        int oldTourNumber = viewModel.CurrentTour;
+
+        // Execute start tour command.
+        viewModel.StartNewTour.Execute(null);
+
+        // Check result.
+        Assert.AreEqual(oldTourNumber + 1, viewModel.CurrentTour);
+    }
+}
