32ec3f41dcf8489d8dcc28b8a11c9ed42a06acba
aleks <aleksandr_nekrasov@outlook.com>
2023-03-23 18:42:35 +0500

    Added patronymic validation rule.

---
+++ ChessTourManager.WPF/Helpers/ValidationRules/PatronymicValidationRule.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace ChessTourManager.WPF.Helpers.ValidationRules;
+
+public class PatronymicValidationRule : ValidationRule
+{
+    public override ValidationResult Validate(object? value, CultureInfo cultureInfo)
+    {
+        string patronymic = value?.ToString() ?? string.Empty;
+
+        if (patronymic.All(char.IsLetter))
+        {
+            return ValidationResult.ValidResult;
+        }
+
+        return new ValidationResult(false, "Неизвестная ошибка!");
+    }
+}
