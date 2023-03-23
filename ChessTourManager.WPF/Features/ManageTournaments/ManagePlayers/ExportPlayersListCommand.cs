5e07be6e44295b4fc9166e18128168a932b4d943
aleks <aleksandr_nekrasov@outlook.com>
2023-03-24 02:34:04 +0500

    Added method for getting chained properties.

--- ChessTourManager.WPF/Features/ManageTournaments/ManagePlayers/ExportPlayersListCommand.cs
+++ ChessTourManager.WPF/Features/ManageTournaments/ManagePlayers/ExportPlayersListCommand.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using ChessTourManager.DataAccess.Entities;
 using ChessTourManager.WPF.Helpers;
 using Microsoft.Win32;
 using OfficeOpenXml;
@@ -51,7 +51,7 @@ public class ExportPlayersListCommand : CommandBase
         catch (Exception e)
         {
             MessageBox.Show(e.Message, "Экспорт списка участников", MessageBoxButton.OK, MessageBoxImage.Error);
-            throw;
+            return;
         }
 
         MessageBox.Show("Данные успешно экспортированы", "Экспорт списка участников",
@@ -68,28 +68,20 @@ public class ExportPlayersListCommand : CommandBase
             sb.Append(column.Header);
             sb.Append(",");
         }
-
         sb.AppendLine();
 
-        // Append data
-        foreach (Player player in dataGrid.Items)
+        List<object> data = dataGrid.ItemsSource.Cast<object>().ToList();
+
+        // Write the data
+        for (var i = 0; i < data.Count; i++)
         {
-            sb.Append(player.PlayerLastName);
-            sb.Append(",");
-            sb.Append(player.PlayerFirstName);
-            sb.Append(",");
-            sb.Append(player.Gender);
-            sb.Append(",");
-            sb.Append(player.PlayerAttribute);
-            sb.Append(",");
-            sb.Append(player.Team?.TeamName);
-            sb.Append(",");
-            sb.Append(player.Group?.GroupName);
-            sb.Append(",");
-            sb.Append(player.PlayerBirthYear);
-            sb.Append(",");
-            sb.Append(player.IsActive);
-            sb.Append(",");
+            object item = data[i];
+            foreach (DataGridColumn col in dataGrid.Columns)
+            {
+                object? value = GetPropertyValue(item, col.SortMemberPath);
+                sb.Append(value);
+                sb.Append(",");
+            }
             sb.AppendLine();
         }
 
@@ -118,22 +110,39 @@ public class ExportPlayersListCommand : CommandBase
         // Write the data
         for (var i = 0; i < data.Count; i++)
         {
-            var item = (Player)data[i];
+            var item = data[i];
             for (var j = 0; j < dataGrid.Columns.Count; j++)
             {
-                object? value = item
-                               .GetType()
-                               .GetProperty(dataGrid.Columns[j].SortMemberPath)?
-                               .GetValue(item, null);
+                object? value = GetPropertyValue(item, dataGrid.Columns[j].SortMemberPath);
                 worksheet.Cells[i + 2, j + 1].Value = value?.ToString();
             }
-
-            worksheet.Cells[i + 2, 5].Value = item.Team?.TeamName;
-            worksheet.Cells[i + 2, 6].Value = item.Group?.GroupName;
-            worksheet.Cells[i + 2, 8].Value = item.IsActive.ToString();
         }
 
         // Save the workbook
         package.SaveAs(new FileInfo(fileName));
+    }
+
+    // Recursive function to get the value of a property with a path that may have multiple levels
+    private object? GetPropertyValue(object obj, string propertyPath)
+    {
+        string[] propertyNames = propertyPath.Split('.');
+        object?  propertyValue = obj;
+
+        foreach (string propertyName in propertyNames)
+        {
+            PropertyInfo? property = propertyValue.GetType().GetProperty(propertyName);
+            if (property == null)
+            {
+                return null;
+            }
+
+            propertyValue = property.GetValue(propertyValue, null);
+            if (propertyValue == null)
+            {
+                return null;
+            }
+        }
+
+        return propertyValue;
     }
 }
