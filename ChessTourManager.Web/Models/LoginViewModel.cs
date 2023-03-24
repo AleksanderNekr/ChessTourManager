f2da0f09f0abe2b252dc8e4488a799f89b76457e
aleks <aleksandr_nekrasov@outlook.com>
2023-03-24 15:06:46 +0500

    Added web project.

---
+++ ChessTourManager.Web/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ChessTourManager.Web.Models;
+
+public class LoginViewModel
+{
+    public string UserName { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
