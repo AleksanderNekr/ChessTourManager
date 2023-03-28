d1d780e8d39e3eaf59de610fedbd1d4852d413a5
aleks <aleksandr_nekrasov@outlook.com>
2023-03-28 23:20:54 +0500

    Added splash screen.

--- ChessTourManager.WPF/App.xaml.cs
+++ ChessTourManager.WPF/App.xaml.cs
@@ -1,6 +1,15 @@
-﻿namespace ChessTourManager.WPF;
+﻿using System.Windows;
+
+namespace ChessTourManager.WPF;
 
 /// <inheritdoc />
 public partial class App
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var splashScreen = new SplashScreen("Assets/ChessTourManagerLogo.png");
+        splashScreen.Show(true);
+
+        base.OnStartup(e);
+    }
 }
