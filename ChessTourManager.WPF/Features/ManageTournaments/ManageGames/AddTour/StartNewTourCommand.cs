dc9312e09b74d01ed6ad788a050c96d8828367e3
aleks <aleksandr_nekrasov@outlook.com>
2023-04-05 13:57:56 +0500

    Removed dummy player.

--- ChessTourManager.WPF/Features/ManageTournaments/ManageGames/AddTour/StartNewTourCommand.cs
+++ ChessTourManager.WPF/Features/ManageTournaments/ManageGames/AddTour/StartNewTourCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using ChessTourManager.DataAccess.Entities;
 using ChessTourManager.DataAccess.Queries.Insert;
@@ -9,106 +10,36 @@ namespace ChessTourManager.WPF.Features.ManageTournaments.ManageGames.AddTour;
 
 public class StartNewTourCommand : CommandBase
 {
-    private static readonly IDrawingAlgorithm DrawingAlgorithm = IDrawingAlgorithm.Initialize(PairsGridViewModel.PairsContext,
-                                                                            TournamentsListViewModel
-                                                                               .SelectedTournament);
+    private static IDrawingAlgorithm _drawingAlgorithm;
 
     private readonly PairsGridViewModel _pairsGridViewModel;
 
     public StartNewTourCommand(PairsGridViewModel pairsGridViewModel)
     {
         _pairsGridViewModel = pairsGridViewModel;
+        _drawingAlgorithm = IDrawingAlgorithm.Initialize(PairsGridViewModel.PairsContext,
+                                                         pairsGridViewModel.OpenedTournament);
     }
 
     public override void Execute(object? parameter)
     {
-        // TODO: fix it
-        // Check if there are any dummy players and make them inactive if there are odd number of players.
-        if (TournamentsListViewModel.SelectedTournament!.Players.Contains(_pairsGridViewModel.DummyPlayer)
-         && (TournamentsListViewModel.SelectedTournament.Players.Count % 2) == 1)
+        if (_pairsGridViewModel.CurrentTour == _pairsGridViewModel.OpenedTournament.ToursCount)
         {
-            _pairsGridViewModel.DummyPlayer.IsActive = false;
-            PairsGridViewModel.PairsContext.SaveChanges();
+            MessageBox.Show("Турнир завершен", "Жеребьевка турнира",
+                            MessageBoxButton.OK, MessageBoxImage.Information);
+            return;
         }
 
-        List<(int, int)> idPairs = new(DrawingAlgorithm.StartNewTour(_pairsGridViewModel.CurrentTour));
+        List<(int, int)> idPairs = new(_drawingAlgorithm.StartNewTour(_pairsGridViewModel.CurrentTour));
 
-        foreach ((int, int) idPair in idPairs)
+        foreach ((int, int) idPair in idPairs.Where(idPair => idPair.Item1 != -1 && idPair.Item2 != -1))
         {
-            Game? game;
-
-            // If id = -1 then it means that the player is absent.
-            // Then add a game with the result of 0:1 and mark as not played.
-            // Also add new dummy player to the database.
-            if (idPair.Item1 == -1)
-            {
-                IInsertQueries.CreateInstance(PairsGridViewModel.PairsContext)
-                              .TryAddPlayer(out Player? dummyPlayer,
-                                            TournamentsListViewModel.SelectedTournament.TournamentId,
-                                            TournamentsListViewModel.SelectedTournament.OrganizerId,
-                                            "<Пусто>", "");
-                if (dummyPlayer is null)
-                {
-                    continue;
-                }
-
-                IInsertQueries.CreateInstance(PairsGridViewModel.PairsContext)
-                              .TryAddGamePair(out game, dummyPlayer.PlayerId, idPair.Item2,
-                                              TournamentsListViewModel.SelectedTournament!
-                                                                      .TournamentId,
-                                              TournamentsListViewModel.SelectedTournament.OrganizerId,
-                                              DrawingAlgorithm.NewTourNumber);
-                if (game != null)
-                {
-                    game.WhitePoints                = 0;
-                    game.BlackPoints                = 1;
-                    game.IsPlayed                   = false;
-                    _pairsGridViewModel.DummyPlayer = dummyPlayer;
-
-                    GameAddedEvent.OnGameAdded(this, new GameAddedEventArgs(game));
-                }
-
-                continue;
-            }
-
-            if (idPair.Item2 == -1)
-            {
-                IInsertQueries.CreateInstance(PairsGridViewModel.PairsContext)
-                              .TryAddPlayer(out Player? dummyPlayer,
-                                            TournamentsListViewModel.SelectedTournament.TournamentId,
-                                            TournamentsListViewModel.SelectedTournament.OrganizerId,
-                                            "<Пусто>", "<Пусто>");
-                if (dummyPlayer is null)
-                {
-                    continue;
-                }
-
-                IInsertQueries.CreateInstance(PairsGridViewModel.PairsContext)
-                              .TryAddGamePair(out game, idPair.Item1, dummyPlayer.PlayerId,
-                                              TournamentsListViewModel.SelectedTournament!
-                                                                      .TournamentId,
-                                              TournamentsListViewModel.SelectedTournament.OrganizerId,
-                                              DrawingAlgorithm.NewTourNumber);
-                if (game != null)
-                {
-                    game.WhitePoints                = 1;
-                    game.BlackPoints                = 0;
-                    game.IsPlayed                   = false;
-                    _pairsGridViewModel.DummyPlayer = dummyPlayer;
-
-                    GameAddedEvent.OnGameAdded(this, new GameAddedEventArgs(game));
-                }
-
-                continue;
-            }
-
             InsertResult result = IInsertQueries.CreateInstance(PairsGridViewModel.PairsContext)
-                                                .TryAddGamePair(out game, idPair.Item1, idPair.Item2,
-                                                                TournamentsListViewModel.SelectedTournament!
-                                                                   .TournamentId,
-                                                                TournamentsListViewModel.SelectedTournament
-                                                                   .OrganizerId,
-                                                                DrawingAlgorithm.NewTourNumber);
+                                                .TryAddGamePair(out Game? game,
+                                                                idPair.Item1, idPair.Item2,
+                                                                _pairsGridViewModel.OpenedTournament.TournamentId,
+                                                                _pairsGridViewModel.OpenedTournament.OrganizerId,
+                                                                _drawingAlgorithm.NewTourNumber);
 
             if (result == InsertResult.Fail)
             {
@@ -118,9 +49,9 @@ public class StartNewTourCommand : CommandBase
                 return;
             }
 
-            GameAddedEvent.OnGameAdded(this, new GameAddedEventArgs(game!));
+            GameAddedEvent.OnGameAdded(this, new GameAddedEventArgs(game));
         }
 
-        TourAddedEvent.OnTourAdded(this, new TourAddedEventArgs(DrawingAlgorithm.NewTourNumber));
+        TourAddedEvent.OnTourAdded(this, new TourAddedEventArgs(_drawingAlgorithm.NewTourNumber));
     }
 }
