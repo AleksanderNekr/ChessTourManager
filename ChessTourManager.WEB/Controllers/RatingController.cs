fced68e8ea085dde13075851f35f14f9f762778f
Александр Некрасов <aleksandr_nekrasov@outlook.com>
2023-05-18 22:04:18 +0500

    [Feature] Added rating controller.

---
+++ ChessTourManager.WEB/Controllers/RatingController.cs
@@ -0,0 +1,57 @@
+﻿using ChessTourManager.DataAccess;
+using ChessTourManager.DataAccess.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChessTourManager.WEB.Controllers;
+
+/// <inheritdoc />
+/// <summary>
+///     The controller for the players of a tournament.
+/// </summary>
+public class RatingController : Controller
+{
+    private static   int              _tournamentId;
+    private static   int              _userId;
+    private readonly ChessTourContext _context;
+
+    /// <summary>
+    ///     The constructor of the controller.
+    /// </summary>
+    /// <param name="context">The context of the database.</param>
+    public RatingController(ChessTourContext context)
+    {
+        this._context = context;
+    }
+
+    /// <summary>
+    ///     GET: Players/Index.
+    /// </summary>
+    /// <returns>Index view.</returns>
+    public async Task<IActionResult> Index(int id)
+    {
+        if (id != 0)
+        {
+            _tournamentId = id;
+        }
+
+        _userId = this._context.Users.First(u => u.UserName == this.User.Identity.Name).Id;
+        await this.LoadTournamentAsync();
+        List<Player> players = await this._context.Players
+                                         .Where(player => player.TournamentId == id)
+                                         .Include(player => player.Team)
+                                         .Include(player => player.Group)
+                                         .OrderByDescending(player => player.PointsAmount)
+                                         .ThenByDescending(player => player.RatioSum1)
+                                         .ThenByDescending(player => player.RatioSum2)
+                                         .ToListAsync();
+        return this.View(players);
+    }
+
+    private async Task LoadTournamentAsync()
+    {
+        this.ViewBag.Tournament = await this._context.Tournaments.FindAsync(_tournamentId, _userId)
+                               ?? throw new NullReferenceException("Tournament not found");
+    }
+}
