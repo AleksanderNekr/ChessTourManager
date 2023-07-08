5f8226f7bfbbf8215b3f8c0723d297f8c12b3196
Александр Некрасов <zapasoiler@gmail.com>
2023-07-09 00:12:19 +0500

    [Feature] Add TourNumber value object.

---
+++ Domain/ValueObjects/TourNumber.cs
@@ -0,0 +1,103 @@
+﻿namespace Domain.ValueObjects;
+
+public readonly struct TourNumber
+{
+    private readonly int _value;
+    private const    int Min = 1;
+    private const    int Max = 20;
+
+    public TourNumber(in int value)
+    {
+        if (value is < Min or > Max)
+        {
+            throw new ArgumentException($"Tour number must be between {Min} and {Max}");
+        }
+
+        this._value = value;
+    }
+
+    public TourNumber NextTourNumber()
+    {
+        if (this._value is Max)
+        {
+            throw new InvalidOperationException("Max tour number reached");
+        }
+
+        return this._value + 1;
+    }
+
+    public TourNumber PreviousTourNumber()
+    {
+        if (this._value is Min)
+        {
+            throw new InvalidOperationException("Min tour number reached");
+        }
+
+        return this._value - 1;
+    }
+
+    public static implicit operator int(in TourNumber tourNumber)
+    {
+        return tourNumber._value;
+    }
+
+    public static implicit operator TourNumber(in int tourNumber)
+    {
+        return new TourNumber(in tourNumber);
+    }
+
+    public override string ToString()
+    {
+        return this._value.ToString();
+    }
+
+    public override int GetHashCode()
+    {
+        return this._value;
+    }
+
+    public static bool operator ==(in TourNumber left, in TourNumber right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(in TourNumber left, in TourNumber right)
+    {
+        return !(left == right);
+    }
+
+    public static bool operator <(in TourNumber left, in TourNumber right)
+    {
+        return left._value < right._value;
+    }
+
+    public static bool operator >(in TourNumber left, in TourNumber right)
+    {
+        return left._value > right._value;
+    }
+
+    public static bool operator <=(in TourNumber left, in TourNumber right)
+    {
+        return left._value <= right._value;
+    }
+
+    public static bool operator >=(in TourNumber left, in TourNumber right)
+    {
+        return left._value >= right._value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is TourNumber other && this.Equals(other);
+    }
+
+    public bool Equals(TourNumber other)
+    {
+        return this._value == other._value;
+    }
+
+    public int CompareTo(TourNumber other)
+    {
+        return this._value.CompareTo(other._value);
+    }
+}
