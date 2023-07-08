daeab862a000c652282fde4132188797009a9b9c
Александр Некрасов <zapasoiler@gmail.com>
2023-07-09 00:13:01 +0500

    [Feature] Add DrawResult enum.

---
+++ Domain/ValueObjects/DrawResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.ValueObjects;
+
+public readonly ref struct DrawResult
+{
+    public enum ResultType
+    {
+        Success,
+        Fail,
+    }
+
+    public required ResultType Type { get; init; }
+
+    public ReadOnlySpan<char> Message { get; init; }
+}
