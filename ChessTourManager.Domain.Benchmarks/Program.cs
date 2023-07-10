1bf1597c2ccfef9a25b146ed38632b9cde2a80b0
Александр Некрасов <zapasoiler@gmail.com>
2023-07-10 17:17:04 +0500

    [Tests] Add Name benchmark.

---
+++ ChessTourManager.Domain.Benchmarks/Program.cs
@@ -0,0 +1,151 @@
+﻿using System.Text.RegularExpressions;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ChessTourManager.Domain.ValueObjects;
+
+namespace ChessTourManager.Domain.Benchmarks;
+
+public class NameWithoutRef
+{
+    private readonly string _value;
+
+    public NameWithoutRef(string value)
+    {
+        value = Regex.Replace(value, @"\s+", " ");
+        ReadOnlySpan<char> trimmed = value.AsSpan().Trim();
+        if (trimmed.Length is < 2 or > 50)
+        {
+            throw new Exception("NameWithRef must be between 2 and 50 characters");
+        }
+
+        this._value = trimmed.ToString();
+    }
+
+    public static implicit operator string(NameWithoutRef name)
+    {
+        return name._value;
+    }
+
+    public static implicit operator NameWithoutRef(string name)
+    {
+        return new NameWithoutRef(name);
+    }
+
+    public override string ToString()
+    {
+        return this._value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not NameWithoutRef other)
+        {
+            return false;
+        }
+
+        return this._value == other._value;
+    }
+
+    public override int GetHashCode()
+    {
+        return this._value.GetHashCode();
+    }
+
+    public static bool operator ==(NameWithoutRef left, NameWithoutRef right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(NameWithoutRef left, NameWithoutRef right)
+    {
+        return !(left == right);
+    }
+}
+
+public class NameWithRef
+{
+    private readonly string _value;
+
+    public NameWithRef(ref string value)
+    {
+        value = Regex.Replace(value, @"\s+", " ");
+        ReadOnlySpan<char> trimmed = value.AsSpan().Trim();
+        if (trimmed.Length is < 2 or > 50)
+        {
+            throw new Exception("NameWithRef must be between 2 and 50 characters");
+        }
+
+        this._value = trimmed.ToString();
+    }
+
+    public static implicit operator string(NameWithRef name)
+    {
+        return name._value;
+    }
+
+    public static implicit operator NameWithRef(string name)
+    {
+        return new NameWithRef(ref name);
+    }
+
+    public override string ToString()
+    {
+        return this._value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not NameWithRef other)
+        {
+            return false;
+        }
+
+        return this._value == other._value;
+    }
+
+    public override int GetHashCode()
+    {
+        return this._value.GetHashCode();
+    }
+
+    public static bool operator ==(NameWithRef left, NameWithRef right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(NameWithRef left, NameWithRef right)
+    {
+        return !(left == right);
+    }
+}
+
+[MemoryDiagnoser]
+public class NameBenchmarks
+{
+    [Params("John Doe", "   Alex   Smith   ", "  John  \t Smith   ")]
+    public string Name;
+
+    [Benchmark]
+    public void NameWithoutRef()
+    {
+        var name = new NameWithoutRef(this.Name);
+
+        _ = name.ToString();
+    }
+
+    [Benchmark]
+    public void NameWithRef()
+    {
+        var name = new NameWithRef(ref this.Name);
+
+        _ = name.ToString();
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<NameBenchmarks>();
+    }
+}
