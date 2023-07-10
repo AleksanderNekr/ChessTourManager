51a4e52c6f3a5bd1157d080651795d75f7bac792
Александр Некрасов <zapasoiler@gmail.com>
2023-07-10 22:42:12 +0500

    [Feature] Add GamePair entity.

---
+++ ChessTourManager.Domain/Entities/GamePair.cs
@@ -0,0 +1,57 @@
+﻿using ChessTourManager.Domain.ValueObjects;
+
+namespace ChessTourManager.Domain.Entities;
+
+public sealed class GamePair : IEquatable<GamePair>
+{
+    public GamePair(in Player white, in Player black)
+    {
+        this.White  = white;
+        this.Black  = black;
+        this.Result = GameResult.NotPlayed;
+    }
+
+    internal Player White { get; }
+
+    internal Player Black { get; }
+
+    internal GameResult Result { get; private set; }
+
+    public bool Equals(GamePair? other)
+    {
+        if (ReferenceEquals(null, other))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+
+        return this.White.Equals(other.White)
+            && this.Black.Equals(other.Black);
+    }
+
+    public override string ToString()
+    {
+        return $"{this.White} - {this.Black}";
+    }
+
+    private void SetResult(in GameResult result)
+    {
+        this.Result = result;
+        this.White.AddGameToHistory(this);
+        this.Black.AddGameToHistory(this);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is GamePair other && this.Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(this.White, this.Black);
+    }
+}
