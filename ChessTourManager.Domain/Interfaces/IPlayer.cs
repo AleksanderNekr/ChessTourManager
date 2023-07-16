47344859bdab2ada308ddcc768c1f8301ab853a8
Александр Некрасов <zapasoiler@gmail.com>
2023-07-16 18:19:16 +0500

    [Feature] Add generic interface IPlayer for playable entities.

---
+++ ChessTourManager.Domain/Interfaces/IPlayer.cs
@@ -0,0 +1,28 @@
+﻿using ChessTourManager.Domain.Entities;
+
+namespace ChessTourManager.Domain.Interfaces;
+
+public interface IPlayer<TPlayer> where TPlayer : IPlayer<TPlayer>
+{
+    decimal Points { get; }
+
+    uint Wins { get; }
+
+    uint Draws { get; }
+
+    uint Loses { get; }
+
+    bool IsActive { get; }
+
+    IEnumerable<TPlayer> GetAllOpponents();
+
+    IEnumerable<TPlayer> GetBlackOpponents();
+
+    IEnumerable<TPlayer> GetWhiteOpponents();
+
+    internal void AddGameToHistory(GamePair<TPlayer> pair, PlayerColor color);
+
+    public void SetActive();
+
+    public void SetInactive();
+}
