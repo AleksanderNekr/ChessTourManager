ce39de4a8be2a71360e29dd60798600a45a7ecdd
Александр Некрасов <zapasoiler@gmail.com>
2023-07-18 13:01:05 +0500

    [Tests] Add tests for DrawResult.

---
+++ ChessTourManager.Domain.ValueObjects.Tests/DrawResultTests.cs
@@ -0,0 +1,58 @@
+﻿public class DrawResultTests
+{
+    [Fact]
+    public void Success_CreatesSuccessDrawResult()
+    {
+        // Arrange
+        ReadOnlySpan<char> message = "Success";
+
+        // Act
+        DrawResult drawResult = DrawResult.Success(message);
+
+        // Assert
+        Assert.Equal(DrawResult.ResultType.Success, drawResult.Result);
+        Assert.Equal(message.ToString(),            drawResult.Message.ToString());
+    }
+
+    [Fact]
+    public void NotEnoughPlayers_CreatesNotEnoughPlayersDrawResult()
+    {
+        // Arrange
+        ReadOnlySpan<char> message = "Not enough players";
+
+        // Act
+        DrawResult drawResult = DrawResult.NotEnoughPlayers(message);
+
+        // Assert
+        Assert.Equal(DrawResult.ResultType.NotEnoughPlayers, drawResult.Result);
+        Assert.Equal(message.ToString(),                     drawResult.Message.ToString());
+    }
+
+    [Fact]
+    public void TournamentIsOver_CreatesTournamentIsOverDrawResult()
+    {
+        // Arrange
+        ReadOnlySpan<char> message = "Tournament is over";
+
+        // Act
+        DrawResult drawResult = DrawResult.TournamentIsOver(message);
+
+        // Assert
+        Assert.Equal(DrawResult.ResultType.TournamentIsOver, drawResult.Result);
+        Assert.Equal(message.ToString(),                     drawResult.Message.ToString());
+    }
+
+    [Fact]
+    public void Fail_CreatesFailDrawResult()
+    {
+        // Arrange
+        ReadOnlySpan<char> message = "Failed to draw";
+
+        // Act
+        DrawResult drawResult = DrawResult.Fail(message);
+
+        // Assert
+        Assert.Equal(DrawResult.ResultType.Fail, drawResult.Result);
+        Assert.Equal(message.ToString(),         drawResult.Message.ToString());
+    }
+}
--- ChessTourManager.Domain.ValueObjects.Tests/Usings.cs
+++ ChessTourManager.Domain.ValueObjects.Tests/Usings.cs
@@ -1,2 +1,3 @@
 global using Xunit;
 global using ChessTourManager.Domain.Exceptions;
+global using ChessTourManager.Domain.ValueObjects;
