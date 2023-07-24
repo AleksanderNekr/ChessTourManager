89a21a856793755a1ec819bae3339d0a67ab4897
Александр Некрасов <zapasoiler@gmail.com>
2023-07-24 22:36:18 +0500

    [Feature] Add TryAdd and TryRemove player methods in Group.cs.

--- ChessTourManager.Domain/Entities/Group.cs
+++ ChessTourManager.Domain/Entities/Group.cs
@@ -5,16 +5,21 @@ namespace ChessTourManager.Domain.Entities;
 
 public sealed class Group : IEquatable<Group>, INameable
 {
-    public Group(Id<Guid> id, Name name, IEnumerable<Player>? players = default)
+    private readonly HashSet<Player> _players;
+
+    internal Group(Id<Guid> id, Name name, HashSet<Player>? players = null)
     {
-        this.Id      = id;
-        this.Name    = name;
-        this.Players = players ?? Enumerable.Empty<Player>();
+        this.Id   = id;
+        this.Name = name;
+        this._players = players ?? new HashSet<Player>(new INameable.ByNameEqualityComparer<Player>());
     }
 
     public Id<Guid> Id { get; }
 
-    public IEnumerable<Player> Players { get; set; }
+    internal IReadOnlySet<Player> Players
+    {
+        get => this._players;
+    }
 
     public Name Name { get; set; }
 
@@ -34,5 +39,34 @@ public sealed class Group : IEquatable<Group>, INameable
     public override int GetHashCode()
     {
         return this.Id.GetHashCode();
+    }
+
+    internal AddPlayerResult TryAddPlayer(Id<Guid> playerId, Name playerName, Gender gender, BirthYear birthYear)
+    {
+        bool isUnique = this._players.Add(new Player(playerId, playerName, gender, birthYear));
+
+        return isUnique
+                   ? AddPlayerResult.Success
+                   : AddPlayerResult.PlayerAlreadyExists;
+    }
+
+    internal RemovePlayerResult TryRemovePlayer(Id<Guid> playerId)
+    {
+        Player? player = this._players.SingleOrDefault(player => player.Id == playerId);
+        if (player is null)
+        {
+            return RemovePlayerResult.PlayerDoesNotExist;
+        }
+
+        if (player.InGames())
+        {
+            return RemovePlayerResult.PlayerInGames;
+        }
+
+        bool isRemoved = this._players.Remove(player);
+
+        return isRemoved
+                   ? RemovePlayerResult.Success
+                   : RemovePlayerResult.Fail;
     }
 }
