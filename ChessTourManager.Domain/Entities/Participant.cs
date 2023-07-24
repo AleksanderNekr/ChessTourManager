54c29e8942ec6b1cff872dee226511017158aae4
Александр Некрасов <zapasoiler@gmail.com>
2023-07-24 22:37:35 +0500

    [Refactor] IPlayer.cs changed to Participant.cs abstract class.

---
+++ ChessTourManager.Domain/Entities/Participant.cs
@@ -0,0 +1,188 @@
+﻿using ChessTourManager.Domain.Exceptions;
+using ChessTourManager.Domain.ValueObjects;
+
+namespace ChessTourManager.Domain.Entities;
+
+public abstract class Participant<TParticipant> where TParticipant : Participant<TParticipant>
+{
+    private decimal _points;
+
+    private readonly HashSet<GamePair<TParticipant>> _gamesHistory;
+
+    protected Participant(Id<Guid> id, Name name, bool isActive)
+    {
+        this.Id       = id;
+        this.Name     = name;
+        this.IsActive = isActive;
+        this._gamesHistory =
+            new HashSet<GamePair<TParticipant>>(comparer: new GamePair<TParticipant>.ByPlayersEqualityComparer());
+    }
+
+    internal Id<Guid> Id { get; }
+
+    public Name Name { get; }
+
+    protected decimal Points
+    {
+        get => this._points;
+        private set
+        {
+            if (value < 0)
+            {
+                throw new DomainException("Points cannot be negative.");
+            }
+
+            this._points = value;
+        }
+    }
+
+    protected uint Wins { get; private set; }
+
+    protected uint Draws { get; private set; }
+
+    protected uint Loses { get; private set; }
+
+    public bool IsActive { get; private set; }
+
+    public IEnumerable<TParticipant> GetAllOpponents()
+    {
+        foreach (GamePair<TParticipant> pair in this._gamesHistory)
+        {
+            if (pair.White.Equals(this))
+            {
+                yield return pair.Black;
+            }
+            else
+            {
+                yield return pair.White;
+            }
+        }
+    }
+
+    public IEnumerable<TParticipant> GetBlackOpponents()
+    {
+        return this._gamesHistory
+                   .Where(pair => pair.White.Equals(this))
+                   .Select(static pair => pair.Black);
+    }
+
+    public IEnumerable<TParticipant> GetWhiteOpponents()
+    {
+        return this._gamesHistory
+                   .Where(pair => pair.Black.Equals(this))
+                   .Select(static pair => pair.White);
+    }
+
+    internal virtual void SetActive()
+    {
+        this.IsActive = true;
+    }
+
+    internal virtual void SetInactive()
+    {
+        this.IsActive = false;
+    }
+
+    internal bool InGames()
+    {
+        return this._gamesHistory.Any();
+    }
+
+    internal void AddGameToHistory(GamePair<TParticipant> pair, PlayerColor color)
+    {
+        if (this.TryAddGamePair(pair, color))
+        {
+            return;
+        }
+
+        this.RemoveGamePair(pair, color);
+        if (!this.TryAddGamePair(pair, color))
+        {
+            throw new DomainException($"Failed to apply new game result: {pair}.");
+        }
+    }
+
+    private protected virtual bool TryAddGamePair(GamePair<TParticipant> pair, PlayerColor color)
+    {
+        if (!this._gamesHistory.Add(pair))
+        {
+            return false;
+        }
+
+        switch (pair.Result)
+        {
+            // Win.
+            case GameResult.WhiteWinByDefault when color == PlayerColor.White:
+            case GameResult.WhiteWin when color          == PlayerColor.White:
+            case GameResult.BlackWinByDefault when color == PlayerColor.Black:
+            case GameResult.BlackWin when color          == PlayerColor.Black:
+                this.Points += 1;
+                this.Wins   += 1;
+
+                break;
+            // Draw.
+            case GameResult.Draw:
+                this.Points += 0.5m;
+                this.Draws  += 1;
+
+                break;
+            // Lose.
+            case GameResult.WhiteWinByDefault:
+            case GameResult.WhiteWin:
+            case GameResult.BlackWinByDefault:
+            case GameResult.BlackWin:
+            // Both leave.
+            case GameResult.BothLeave:
+                this.Loses += 1;
+
+                break;
+            case GameResult.NotYetPlayed:
+                break;
+            default:
+                throw new DomainOutOfRangeException(nameof(pair.Result), pair.Result);
+        }
+
+        return true;
+    }
+
+    private protected virtual void RemoveGamePair(GamePair<TParticipant> pair, PlayerColor color)
+    {
+        if (!this._gamesHistory.Remove(pair))
+        {
+            throw new DomainException($"Failed to remove game result: {pair}, because it is not found.");
+        }
+
+        switch (pair.Result)
+        {
+            // Was win.
+            case GameResult.WhiteWinByDefault when color == PlayerColor.White:
+            case GameResult.WhiteWin when color          == PlayerColor.White:
+            case GameResult.BlackWinByDefault when color == PlayerColor.Black:
+            case GameResult.BlackWin when color          == PlayerColor.Black:
+                this.Points -= 1;
+                this.Wins   -= 1;
+
+                break;
+            // Was draw.
+            case GameResult.Draw:
+                this.Points -= 0.5m;
+                this.Draws  -= 1;
+
+                break;
+            // Was lose.
+            case GameResult.WhiteWinByDefault:
+            case GameResult.WhiteWin:
+            case GameResult.BlackWinByDefault:
+            case GameResult.BlackWin:
+            // Was both leave.
+            case GameResult.BothLeave:
+                this.Loses -= 1;
+
+                break;
+            case GameResult.NotYetPlayed:
+                break;
+            default:
+                throw new DomainOutOfRangeException(nameof(pair.Result), pair.Result);
+        }
+    }
+}
--- ChessTourManager.Domain/Entities/Player.cs
+++ ChessTourManager.Domain/Entities/Player.cs
@@ -1,47 +1,20 @@
-﻿using ChessTourManager.Domain.Exceptions;
-using ChessTourManager.Domain.Interfaces;
+﻿using ChessTourManager.Domain.Interfaces;
 using ChessTourManager.Domain.ValueObjects;
 
 namespace ChessTourManager.Domain.Entities;
 
-public sealed class Player : IEquatable<Player>, INameable, IPlayer<Player>
+public sealed class Player : Participant<Player>, IEquatable<Player>, INameable
 {
-    private readonly HashSet<GamePair<Player>> _gamesHistory;
-    private          decimal                   _points;
-
-    public Player(Id<Guid> id, Name name)
+    internal Player(Id<Guid> id, Name name, Gender gender, BirthYear birthYear, bool isActive = true)
+        : base(id, name, isActive)
     {
-        this.Id            = id;
-        this.Name          = name;
-        this.IsActive      = true;
-        this._gamesHistory = new HashSet<GamePair<Player>>(comparer: new GamePair<Player>.ByPlayersEqualityComparer());
+        this.PlayerGender = gender;
+        this.BirthYear    = birthYear;
     }
 
-    private Id<Guid> Id { get; }
+    public Gender PlayerGender { get; set; }
 
-    public Name Name { get; }
-
-    public decimal Points
-    {
-        get => this._points;
-        private set
-        {
-            if (value < 0)
-            {
-                throw new DomainException("Points cannot be negative.");
-            }
-
-            this._points = value;
-        }
-    }
-
-    public uint Wins { get; private set; }
-
-    public uint Draws { get; private set; }
-
-    public uint Loses { get; private set; }
-
-    public bool IsActive { get; private set; }
+    public BirthYear BirthYear { get; set; }
 
     public bool Equals(Player? other)
     {
@@ -63,35 +36,6 @@ public sealed class Player : IEquatable<Player>, INameable, IPlayer<Player>
             && this.Loses  == other.Loses;
     }
 
-    public IEnumerable<Player> GetAllOpponents()
-    {
-        foreach (GamePair<Player> pair in this._gamesHistory)
-        {
-            if (pair.White.Equals(this))
-            {
-                yield return pair.Black;
-            }
-            else
-            {
-                yield return pair.White;
-            }
-        }
-    }
-
-    public IEnumerable<Player> GetBlackOpponents()
-    {
-        return this._gamesHistory
-                   .Where(pair => pair.White.Equals(this))
-                   .Select(static pair => pair.Black);
-    }
-
-    public IEnumerable<Player> GetWhiteOpponents()
-    {
-        return this._gamesHistory
-                   .Where(pair => pair.Black.Equals(this))
-                   .Select(static pair => pair.White);
-    }
-
 
     public override string ToString()
     {
@@ -107,112 +51,10 @@ public sealed class Player : IEquatable<Player>, INameable, IPlayer<Player>
     {
         return this.Id.GetHashCode();
     }
+}
 
-    public void AddGameToHistory(GamePair<Player> pair, PlayerColor color)
-    {
-        if (this.TryAddGamePair(pair, color))
-        {
-            return;
-        }
-
-        this.RemoveGamePair(pair, color);
-        if (!this.TryAddGamePair(pair, color))
-        {
-            throw new DomainException($"Failed to apply new game result: {pair}.");
-        }
-    }
-
-    public void SetActive()
-    {
-        this.IsActive = true;
-    }
-
-    public void SetInactive()
-    {
-        this.IsActive = false;
-    }
-
-    private bool TryAddGamePair(GamePair<Player> pair, PlayerColor color)
-    {
-        if (!this._gamesHistory.Add(pair))
-        {
-            return false;
-        }
-
-        switch (pair.Result)
-        {
-            // Win.
-            case GameResult.WhiteWinByDefault when color == PlayerColor.White:
-            case GameResult.WhiteWin when color          == PlayerColor.White:
-            case GameResult.BlackWinByDefault when color == PlayerColor.Black:
-            case GameResult.BlackWin when color          == PlayerColor.Black:
-                this.Points += 1;
-                this.Wins   += 1;
-
-                break;
-            // Draw.
-            case GameResult.Draw:
-                this.Points += 0.5m;
-                this.Draws  += 1;
-
-                break;
-            // Lose.
-            case GameResult.WhiteWinByDefault:
-            case GameResult.WhiteWin:
-            case GameResult.BlackWinByDefault:
-            case GameResult.BlackWin:
-            // Both leave.
-            case GameResult.BothLeave:
-                this.Loses += 1;
-
-                break;
-            case GameResult.NotYetPlayed:
-                break;
-            default:
-                throw new DomainOutOfRangeException(nameof(pair.Result), pair.Result);
-        }
-
-        return true;
-    }
-
-    private void RemoveGamePair(GamePair<Player> pair, PlayerColor color)
-    {
-        if (!this._gamesHistory.Remove(pair))
-        {
-            throw new DomainException($"Failed to remove game result: {pair}, because it is not found.");
-        }
-
-        switch (pair.Result)
-        {
-            // Was win.
-            case GameResult.WhiteWinByDefault when color == PlayerColor.White:
-            case GameResult.WhiteWin when color          == PlayerColor.White:
-            case GameResult.BlackWinByDefault when color == PlayerColor.Black:
-            case GameResult.BlackWin when color          == PlayerColor.Black:
-                this.Points -= 1;
-                this.Wins   -= 1;
-
-                break;
-            // Was draw.
-            case GameResult.Draw:
-                this.Points -= 0.5m;
-                this.Draws  -= 1;
-
-                break;
-            // Was lose.
-            case GameResult.WhiteWinByDefault:
-            case GameResult.WhiteWin:
-            case GameResult.BlackWinByDefault:
-            case GameResult.BlackWin:
-            // Was both leave.
-            case GameResult.BothLeave:
-                this.Loses -= 1;
-
-                break;
-            case GameResult.NotYetPlayed:
-                break;
-            default:
-                throw new DomainOutOfRangeException(nameof(pair.Result), pair.Result);
-        }
-    }
+public enum Gender
+{
+    Male,
+    Female,
 }
